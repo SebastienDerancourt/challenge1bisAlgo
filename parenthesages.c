17c0671f27162b71270d3764c1031edfbc87e631
Sébastien Derancourt <sebastien.derancourt@gmail.com>
2025-12-14 15:29:47 +0100

    code reviewed and corrected

--- parenthesages.c
+++ parenthesages.c
@@ -1,72 +1,68 @@
-//
-//  parenthesages.c
-//  
-//  Adapté pour l'exercice challenge
-//
-
 #include <stdio.h>
-#include <limits.h> // Pour INT_MAX
+#include <limits.h>
 #include "mymatrix.h"
 
+// initialisation de A
 void init_A(int N[], int n, MyMatrix A)
 {
     int i ;
-    // Initialisation de la diagonale de A à 0
-    // (Le coût de multiplication d'une seule matrice est nul)
+    // initialisation de la diagonale de A à 0
     for (i = 1; i <= n; i++) {
         set_matrix(A, i, i, 0);
     }
 }
 
+// remplissage de A/B par ligne
 void compute_A_B(int N[], int n, MyMatrix A, MyMatrix B)
 {
-    int i, j, k, L ;
-    int q ;
+    int i, j, k ;
+    int min_val, min_pos, tmp ;
     
-    // Remplissage de A et B
-    // On procède par diagonales : d'abord les chaînes de longueur L=2, puis L=3, etc.
-    for (L = 2; L <= n; L++) {
-        for (i = 1; i <= n - L + 1; i++) {
-            j = i + L - 1; // j est l'indice de fin de la chaîne
-            
-            // On initialise le coût à l'infini pour trouver le minimum
-            set_matrix(A, i, j, INT_MAX);
-            
-            // On teste toutes les coupures k possibles entre i et j-1
-            for (k = i; k <= j - 1; k++) {
-                // Formule de récurrence : coût gauche + coût droit + coût combinaison
-                // Dimensions : (N[i] x N[k+1]) * (N[k+1] x N[j+1])
-                q = get_matrix(A, i, k) + get_matrix(A, k + 1, j) + N[i] * N[k+1] * N[j+1];
-                
-                // Si on trouve un coût plus petit, on met à jour
-                if (q < get_matrix(A, i, j)) {
-                    set_matrix(A, i, j, q);
-                    set_matrix(B, i, j, k); // On sauvegarde l'indice de coupure k
+    for (i = n - 1; i >= 1; --i)
+    {
+        for (j = i + 1; j <= n; ++j)
+        {
+            // A [i][i] = 0
+            // cout = A[i+1][j] + cout de multiplication
+            min_val = get_matrix(A, i, i) + get_matrix(A, i + 1, j) + N[i] * N[i+1] * N[j+1];
+            min_pos = i; // premiere coupure possible 
+
+            // teste des coupures k entre i+1 et j-1
+            for (k = i + 1; k < j; ++k)
+            {
+                tmp = get_matrix(A, i, k) + get_matrix(A, k + 1, j) + N[i] * N[k+1] * N[j+1]; // p(i, k) + p(k+1, j) + dim
+
+                if (tmp < min_val)
+                {
+                    min_val = tmp;
+                    min_pos = k;
                 }
             }
+
+            set_matrix(A, i, j, min_val);
+            set_matrix(B, i, j, min_pos);
         }
     }
 }
 
+// calcul du parenthesage optimal 
 void compute_opt_paren(MyMatrix B, int i, int j)
 {
     int k ;
-    // Conditions d'arrêt : si i == j, on affiche juste l'indice de la matrice
-    if (i == j) {
+    if (i == j)
         printf("%d", i);
-    }
     else
     {
         printf("(");
-        // On récupère le k optimal stocké dans B
+        
         k = get_matrix(B, i, j);
         
-        // Appel récursif pour la partie gauche
+        // partie gauche
         compute_opt_paren(B, i, k);
         
         printf(",");
         
-        // Appel récursif pour la partie droite
+        // partie droite
         compute_opt_paren(B, k + 1, j);
         printf(")");
     }
@@ -75,47 +71,41 @@ void compute_opt_paren(MyMatrix B, int i, int j)
 
 int main ()
 {
-    // Configuration pour le challenge (j) : 10 matrices
-    const int n = 10;
+    int ns[] = {3, 10}; // nombre de matrices
+    int N3[]  = {0, 3, 2, 4, 5}; // tailles pour n = 3 (Question 3.i)
+    int N10[] = {0, 3, 8, 4, 5, 2, 10, 20, 4, 3, 6, 12}; // tailles pour n = 10 (Question 3.j)
+
+    int *testsN[] = {N3, N10};
     
-    // Configuration pour le test (i) : 3 matrices (à décommenter pour tester)
-    // const int n = 3;
-    
-    // Codage des tailles de matrices par un tableau N
-    // Indices de 0 à n+1 pour correspondre à l'énoncé (N[i] x N[i+1])
-    
-    // Pour n=10
-    int N[] = {0, 3, 8, 4, 5, 2, 10, 20, 4, 3, 6, 12};
-    
-    // Pour n=3 (à décommenter pour tester)
-    // int N[] = {0, 3, 2, 4, 5};
-    
-    // Matrices A et B de taille (n+1)x(n+1)
-    // On utilise les indices de 1 à n, donc il faut allouer n+1
-    MyMatrix A = create_matrix(n + 1, n + 1);
-    MyMatrix B = create_matrix(n + 1, n + 1);
-    
-    // Initialisation de A
-    init_A(N, n, A);
-    
-    // Remplissage de A/B avec l'algo de programmation dynamique
-    compute_A_B(N, n, A, B);
-    
-    // Affichage des matrices (optionnel pour les grandes tailles)
-    printf("Matrice A (Coûts) :\n") ;
-    print_matrix(A) ;
-    // printf("Matrice B (Découpes) :\n") ;
-    // print_matrix(B) ;
-    
-    // Résultat final
-    printf("Parenthésage optimal :\n") ;
-    compute_opt_paren(B, 1, n);
-    printf("\n") ;
-    printf("%d multiplications\n", get_matrix(A, 1, n)) ;
-    
-    // Nettoyage mémoire
-    free_matrix(A);
-    free_matrix(B);
-    
+
+    for (int t = 0; t < 2; ++t) { // boucle pour faire les test sur 3 puis 10 matrices
+        int n = ns[t];
+        int *N = testsN[t];
+
+        printf("\e[4;32mTest sur %d matrices (Question 3.%c):\e[0m\n\n", n, 'i' + t);
+
+        // sujet: Vous utiliserez des matrices de taille (n + 1) × (n + 1) dont vous n’utiliserez pas la première ligne/colonne (d’indice 0).
+        MyMatrix A = create_matrix(n + 1, n + 1);
+        MyMatrix B = create_matrix(n + 1, n + 1);
+
+        init_A(N, n, A);
+        compute_A_B(N, n, A, B);
+
+        printf("Matrice A (Coûts): \n");
+        print_matrix(A);
+        printf("\n");
+        printf("Matrice B (Découpes): \n");
+        print_matrix(B);
+        printf("\n");
+
+        printf("Parenthésage optimal: ");
+        compute_opt_paren(B, 1, n);
+        printf("\n");
+        printf("Nombre de produits nécessaires: %d\n\n", get_matrix(A, 1, n));
+
+        free_matrix(A);
+        free_matrix(B);
+    }
+
     return 0 ;
 }
